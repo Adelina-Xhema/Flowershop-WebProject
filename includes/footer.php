6a6eba1c35fb14b62081fd6dbfa2d33bfe97b612
adelina <93861676+Adelina-Xhema@users.noreply.github.com>
2022-03-11 12:44:08 +0100

    footer

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+ <div class="footer-div">
+    <div class="lista">
+      <a href="https://www.instagram.com/">Instagram</a>
+      <br>
+      <a href="https://www.facebook.com/">Facebook</a>
+      <br>
+      <a href="https://www.Twitter.com/">Twitter</a>
+    </div>
+    
+    <div class="orari">
+      <ul type="none">
+        <li><b>Open hourse </b></li>
+        <li>Monday to Saturday: 9:00-18:30 </li>
+        <li>Online : 24h</li>
+      </ul>
+    </div>
+    <div class="adresa">
+      <ul class="kon">
+        <ul type="none">
+          <li>Adresa : Qyteza Pejton Prishtine</li>
+          <li>Nr Telefonit : 044-111-111</li>
+          <li>Email Adresa : FlowerShop@gmail.com</li>
+        </ul>
+      </div>
+    </div>
