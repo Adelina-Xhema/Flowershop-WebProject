d1fd818a053f002b8e9e5522ff62835e7d3f6402
adelina <93861676+Adelina-Xhema@users.noreply.github.com>
2022-03-11 12:44:42 +0100

    header

---
+++ includes/header.php
@@ -0,0 +1,62 @@
+<!--<div class="header-div">
+      <div class="img-div">
+        <a href="index.php"><img src="images/flower1.png" alt="" >
+        </div>
+        <div class="list-div">
+          <ul class="nav-list">
+            <li>
+              <a href="index.php">Home</a>
+            </li>
+            <li>
+              <a href="produktet.php">Products</a>
+            </li>
+            <li>
+              <a href="about.php">About</a>
+            </li>
+            <li>
+              <a href="contact.php">Contact</a>
+            </li>
+            <li>
+              <a href="signup.php">Sign up</a>
+            </li>
+            <li>
+              <a href="login.php">Log in</a>
+            </li>
+           <li>
+              <a href="logout.php">Log out</a>
+            </li> 
+           
+          </ul>
+        </div>
+      </div>-->
+      <div class="header-div">
+      <div class="img-div">
+        <a href="index.php"><img src="images/flower1.png" alt="" >
+        </div>
+        <div class="list-div">
+          <ul class="nav-list">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="produktet.php">Products</a></li>
+            <li> <a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            
+      <?php 
+        if(isset($_SESSION['roli']) && $_SESSION['roli'] == 1){
+        ?>
+        <li><a href="views/dashboard.php">Dashboard</a></li>
+        <?php
+        }
+        ?>
+
+        <?php if(isset($_SESSION['roli'])): ?>
+        <li><a href="logout.php">Logout</a></li>
+    <?php else: ?>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="signup.php">Signup</a></li>
+    <?php endif; ?>
+
+</ul>
+
+  </div>
+      </div> 
+      
